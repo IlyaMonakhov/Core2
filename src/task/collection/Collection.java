81a386f43ba5fce3c0e61c259293d1fc64baf6ab
IlyaMonakhov <asd46i@mail.ru>
2024-01-29 18:07:18 +0300

    Test

--- src/task/collection/Collection.java
+++ src/task/collection/Collection.java
@@ -1,19 +1,18 @@
 package task.collection;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
+import java.util.Map;
+
 
 public class Collection {
     public static void main(String[] args) {
-        ArrayList<Integer> listNumbers = new ArrayList<>();
-        listNumbers.add(12);
-        listNumbers.add(55);
-        listNumbers.add(33);
-        listNumbers.add(33);
-        listNumbers.add(22);
-        listNumbers.add(46);
+        ArrayList<Integer> ListNumbers = new ArrayList<>();
+        ListNumbers.add(12);
+        ListNumbers.add(55);
+        ListNumbers.add(33);
+        ListNumbers.add(33);
+        ListNumbers.add(22);
+        ListNumbers.add(46);
         //System.out.println(maxNumbers(listNumbers));
         //removeDuplicates(listNumbers);
         //System.out.println(listNumbers);
@@ -22,17 +21,43 @@ public class Collection {
         //int element = 46;
         //contentElement(listNumbers, element);
         //System.out.println(listNumbers.contains(element));
-        Set<String> setNames = new HashSet<>();
-        setNames.add("Zaur");
-        setNames.add("Marina");
-        setNames.add("Igor");
-        setNames.add("Misha");
-        Set<String> setNamesTwo = new HashSet<>();
-        setNamesTwo.add("Ilya");
-        setNamesTwo.add("Egor");
-        setNamesTwo.add("Denis");
+        Set<String> SetNamesFirst = new HashSet<>();
+        SetNamesFirst.add("Zaur");
+        SetNamesFirst.add("Marina");
+        SetNamesFirst.add("Igor");
+        SetNamesFirst.add("Misha");
+        Set<String> SetNamesSecond = new HashSet<>();
+        SetNamesSecond.add("Ilya");
+        SetNamesSecond.add("Egor");
+        SetNamesSecond.add("Denis");
         //System.out.println(setNames.contains("Zaur"));
         //System.out.println(intersection(setNames , setNamesTwo));
+
+        Map<Integer , String> PassportСontrol = new HashMap<>();
+        PassportСontrol.put(125 , "Ivan");
+        PassportСontrol.put(458 , "Ilya");
+        PassportСontrol.put(555 , "Igor");
+        PassportСontrol.put(666 , "Egor");
+        PassportСontrol.put(777 , "Masha");
+        System.out.println(PassportСontrol.containsKey(666));
+        System.out.println(PassportСontrol.get(555));
+        removePassportСontrol(PassportСontrol);
+        System.out.println(PassportСontrol);
+        Stack<String> Details = new Stack<>();
+        Details.push("engine");
+        Details.push("wheel");
+        Details.push("mirror");
+        Details.push("pin");
+        System.out.println(Details);
+        addStack(Details);
+        System.out.println(Details);
+        popStack(Details);
+        System.out.println(Details);
+        isEmptyStack(Details);
+        System.out.println(Details.isEmpty());
+
+
+
 
 
 
@@ -73,32 +98,48 @@ public class Collection {
     }
 
     // TODO: Найти пересечение двух множеств.
-    public static boolean intersection(Set setNames , Set setNamesTwo){
-        return setNamesTwo.retainAll(setNames);
+    public static boolean intersection(Set SetNamesFirst , Set SetNamesSecond){
+        return SetNamesSecond.retainAll(SetNamesFirst);
     }
 
     // TODO: Удалить все элементы из множества, которые также содержатся в другом множестве.
-    public static void  recurringRemoval(Set setNames , Set setNamesTwo){
-       setNames.removeAll(setNamesTwo);
+    public static void  recurringRemoval(Set SetNamesFirst , Set SetNamesSecond){
+        SetNamesFirst.removeAll(SetNamesSecond);
     }
 
     // TODO: Проверить, содержит ли карта определенный ключ.
-
-
+    public static boolean сheckingKey (Map<Integer , String> PassportСontrol){
+        return PassportСontrol.containsKey(666);
+    }
     // TODO: Получить все значения, связанные с определенным ключом.
+    public static String getPassportСontrol(Map<Integer , String> PassportСontrol) {
+        return PassportСontrol.get(555);
+    }
 
     // TODO: Удалить все записи из карты, у которых значение равно определенному значению.
+    public static void removePassportСontrol(Map<Integer , String> PassportСontrol){
+        PassportСontrol.remove(777);
+    }
 
     // TODO: Добавить элемент в очередь.
+    public static void addStack(Stack<String> Details){
+        Details.push("brakes");
+    }
 
     // TODO: Получить и удалить первый элемент из очереди.
-
+    public static void popStack(Stack<String> Details){
+        Details.pop();
+    }
     // TODO: Проверить, пуста ли очередь.
+    public static boolean isEmptyStack(Stack<String> Details){
+        return Details.isEmpty();
+    }
 
     // TODO: Учет заказов:
     //  Создайте класс Order с полями id, customer и totalAmount.
     //  Создайте метод, который будет принимать список заказов и выводить заказы с наибольшей суммой.
     //  Создайте метод, который будет принимать список заказов и возвращать общую сумму всех заказов.
+
 
     // TODO: Управление задачами:
     //  Создайте класс Task с полями id, title и completed.
